b0052b10f305e7eae435f7049cc61461a8f0d939
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-01-17 14:27:31 -0300

    refactor: move all errors report to an single file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,4 @@
-import { InvalidParamError } from '../errors/invalid-params-error'
-import { MissingParamError } from '../errors/missign-params-error'
+import { InvalidParamError, MissingParamError } from '../errors'
 import { badRequest, serverError } from '../helper/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email--validator'
---
+++ src/presentation/errors/index.ts
@@ -0,0 +1,3 @@
+export * from './invalid-params-error'
+export * from './missign-params-error'
+export * from './server-error'
