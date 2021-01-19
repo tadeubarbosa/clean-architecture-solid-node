f00bffc4ff306d466bc437f394db091f52d326a4
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-01-19 07:58:06 -0300

    feat: ensure DbAddAccount calls AddAccountRepositiry with correct values

--- src/data/useCases/add-account/db-add-account-protocols.ts
+++ src/data/useCases/add-account/db-add-account-protocols.ts
@@ -3,3 +3,4 @@
 export * from '../../../domain/models/account'
 export * from '../../../domain/useCases/adds-acount'
 export * from '../../protocols/encrypter'
+export * from '../../protocols/add-acccount-repository'
