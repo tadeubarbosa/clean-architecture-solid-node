731738d4574f682f3dbf0ba1d793005df8eba767
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-02-05 11:16:27 -0300

    feat: add SigunpFactory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,14 @@
+import { DbAddAccount } from '../../data/useCases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { SignupController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+
+export const makeSignupController = (): SignupController => {
+  const salt = 12
+  const emailValidator = new EmailValidatorAdapter()
+  const encrypter = new BcryptAdapter(salt)
+  const addAccountRepository = new AccountMongoRepository()
+  const addAccount = new DbAddAccount(encrypter, addAccountRepository)
+  return new SignupController(emailValidator, addAccount)
+}
